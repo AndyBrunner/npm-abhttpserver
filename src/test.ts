a882c765877f118f355f673b71916286ad55930e
Andy Brunner <andy.brunner@abdata.ch>
2018-10-31 14:49:44 +0100

    Version 2.0.0

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,14 @@
 /* ABHttpServer Test */
 
-import {ABHttpServer} from './ABHttpServer'
+import { ABHttpServer, ABRequest} from './ABHttpServer'
+import { ServerResponse } from 'http';
 
 class MyServer extends ABHttpServer {
-  get(url: string, request: any, response: any) {
-    this.sendText(response, `Hello - The URL sent was ${url}`)
+  get(request: ABRequest, response: ServerResponse) {
+    this.sendText(response, `Hello - The URL sent was ${request.url.path}`)
+  }
+  post(request: ABRequest, response: ServerResponse) {
+    this.sendJSON(response, { data: `The data sent was ${request.http.data}` })
   }
 }
 var myServer = new MyServer(8080, 8081)
